a08fdcd3a8100d00defd6f2fdf2eef32e043ffbe
Gabriel <gabrieldsilva@protonmail.com>
2023-04-08 04:09:59 -0300

    Code Update

---
+++ report/figures/plot_style.py
@@ -0,0 +1,8 @@
+plt.style.use(['seaborn-v0_8-darkgrid', 'seaborn-v0_8-dark-palette'])
+plt.rcParams.update({
+    'font.family': 'serif',
+    'font.size': 12,
+    'pgf.texsystem': 'lualatex',
+    'pgf.preamble':  r'\usepackage[default]{fontsetup}',
+    'pgf.rcfonts' : False
+})
