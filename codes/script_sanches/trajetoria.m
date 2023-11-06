52c811060ee5b1ae1ef1605ea8d31ae37c843d91
Gabriel <gabrieldsilva@protonmail.com>
2023-11-06 01:20:42 -0500

    Code Update

--- codes/script_sanches/trajetoria.m
+++ codes/script_sanches/trajetoria.m
@@ -1,4 +1,4 @@
-function [x_des,y_des,z_des,x_dot_des,y_dot_des,z_dot_des]=trajetoria(t,option, var)
+function [x_des,y_des,z_des,x_dot_des,y_dot_des,z_dot_des]=trajetoria(t,option)
 
 if option == 1
     
@@ -43,8 +43,8 @@ end
 
 elseif option ==2
     
-    x_des=0.5*cos(t/2) + var/600;
-    y_des=0.5*sin(t/2) + var/600;
+    x_des=0.5*cos(t/2) + rand/1000;
+    y_des=0.5*sin(t/2) + rand/1000;
     z_des=3*ones(1,length(t));
 %     z_des=1+t/10;
     x_dot_des=-0.25*sin(t/2);
